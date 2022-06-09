bc1ce7020cb8f5af9dee345c07d1b59be397e31d
Michel Filipe <eu@michel.io>
2022-06-09 00:17:54 -0300

    output formata dataframe pronto para já exportar
    - agora os registros são ordenados pela coluna magic formula
    - output.to_csv removido por não ter mais utilidade

--- src/output.py
+++ src/output.py
@@ -1,13 +1,11 @@
 import pandas as pd
 
-def format_df(df_full):
+def get_formatted_df(df_full):
   return pd.DataFrame(df_full, 
                       columns=['TICKER','PRECO','P/L','DIVIDA LIQUIDA / EBIT','P. LIQ. / PASSIVO','ROE','ROIC',' LPA',
                                'EY','RANK EY','RANK ROIC','MF',
                                'PROVENTOS 3ANOS','PROVENTOS 5ANOS',
                                'DY','PRECO-TETO DY ATUAL','PRECO-TETO DY 5%','PRECO-TETO DY 10%',
                                'PAYOUT 2021','PAYOUT 2020','PAYOUT 2019','PAYOUT 2018','PAYOUT 2017',
-                               'ATIVO','PASSIVO','PATRIMONIO LÍQUIDO'])
-
-def to_csv(df, filepath='output.csv'):
-  format_df(df).set_index('TICKER').to_csv(filepath)
+                               'ATIVO','PASSIVO','PATRIMONIO LÍQUIDO']).set_index('TICKER') \
+                                                                       .sort_values('MF', ascending=True)
