422bb28197007d4979a561863df94f16bc45712c
Michel Filipe <eu@michel.io>
2022-06-09 02:08:41 -0300

    adiciona indicadores de crescimento (cagr)

--- src/output.py
+++ src/output.py
@@ -4,6 +4,7 @@ def get_formatted_df(df_full):
   return pd.DataFrame(df_full, 
                       columns=['TICKER','PRECO','P/L','DIVIDA LIQUIDA / EBIT','P. LIQ. / PASSIVO','ROE','ROIC',' LPA',
                                'EY','RANK EY','RANK ROIC','MF',
+                               'CAGR RECEITAS 5 ANOS','CAGR LUCROS 5 ANOS',
                                'PROVENTOS 3ANOS','PROVENTOS 5ANOS',
                                'DY','PRECO-TETO DY ATUAL','PRECO-TETO DY 5%','PRECO-TETO DY 10%',
                                'PAYOUT 2021','PAYOUT 2020','PAYOUT 2019','PAYOUT 2018','PAYOUT 2017',
