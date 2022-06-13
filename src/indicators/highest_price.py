d52f69e040e51ea5acb80372c56352ce282f69b5
Michel Filipe <eu@michel.io>
2022-06-12 23:19:34 -0300

    sem necessidade highest_price ser definido em loop

--- src/indicators/highest_price.py
+++ src/indicators/highest_price.py
@@ -2,13 +2,12 @@ import pandas as pd
 import numpy as np
 
 def get_by_dy_df(tickers, df_provents, df_stocks):
+  highest_price = lambda provents, dy: provents / (dy / 100)
+  
   df_rows = []
-
   for ticker in tickers:
     provents_1y = df_provents[df_provents['TICKER'] == ticker]['PROVENTOS 1 ANO'].iat[0]
     provents_5y = df_provents[df_provents['TICKER'] == ticker]['PROVENTOS 5 ANOS'].iat[0]
-
-    highest_price = lambda provents, dy: provents / (dy / 100)
     
     df_rows.append([ticker, highest_price(provents_1y, df_stocks[df_stocks['TICKER'] == ticker]['DY'].iat[0]), 
                     highest_price(provents_5y, 5), highest_price(provents_5y, 10)])
